342986a7c1fd80e9f2b09de969872faf71a0ac2d
Soykipyegon <ksoy2871@gmail.com>
2022-12-08 06:42:53 -0800

    Error task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 /**
- *main - computes sizes of various data types
- *Return: zero
+ *main - print the string into the put function
+ *
+ *Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-	printf("Size of a char: %zu byte\(s\)\n", sizeof(c));
-        printf("Size of an int: %zu byte\(s\)\n", sizeof(i));
-        printf("Size of a long int: %zu byte\(s\)\n", sizeof(long int));
-        printf("Size of a long long int: %zu byte\(s\)\n", sizeof(long long int));
-        printf("Size of a float: %zu byte\(s\)\n", sizeof(f));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;  
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
-
 }
