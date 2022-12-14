41a5391e136b9c9194811d50f8a369bd84366883
Soykipyegon <ksoy2871@gmail.com>
2022-12-14 07:39:20 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abc - computes the absolute value of an interger
+ * @c: The number to be computed
+ * Return: Absolute value of number or zero
+ */
+
+int _abc(int c)
+{ 
+
+	if (c < 0)
+	{
+		int abs_val;
+
+		abc_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
