5caff5afce17de5e447e1ab4ebc7a2b09f1a6fa5
Soykipyegon <ksoy2871@gmail.com>
2022-12-21 14:38:42 -0800

    Advance task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,18 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * print_number - prints a number
- * @n: Input number
- */
 
+/**
+ * print_number - prints an interger.
+ * @n: interger.
+ * Return: nothing.
+ */
 void print_number(int n)
 {
-int res, temp, expo;
-
-expo = 1;
-/*Check negatives*/
-if (n >= 0)
-res = n * -1;
-else
+if (n < 0)
 {
-res = n;
 _putchar('-');
+n = -n;
 }
-
-/*Initialize exponent variable*/
-temp = res;
-while (temp <= -10)
-{
-expo *= 10;
-temp /= 10;
-}
-/*Main */
-while (expo >= 1)
-{
-_putchar(((res / expo) % 10) * -1 + '0');
-expo /= 10;
-}
+if (n / 10)
+print_number(n / 10);
+_putchar('0' + n % 10);
 }
