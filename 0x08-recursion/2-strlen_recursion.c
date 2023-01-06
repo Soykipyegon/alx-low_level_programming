d0fe2699d9a070c81c5493d0b1860aa4d5b8b782
Soykipyegon <ksoy2871@gmail.com>
2023-01-06 09:43:34 -0800

    Mandatory task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Calculates the length of a string
+ *@s: string to calculate the length
+ *Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
