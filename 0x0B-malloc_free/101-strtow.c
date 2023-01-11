caca016ab1abf2b5a2bb27c9b8487cb88573c8ce
Soykipyegon <ksoy2871@gmail.com>
2023-01-11 08:21:13 -0800

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -49,104 +49,54 @@ index += word_len(str + index);
 return (words);
 }
 
-
-
 /**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
  *
- *  * strtow - Splits a string into words.
- *
- *   * @str: The string to be split.
- *
- *    *
- *
- *     * Return: If str = NULL, str = "", or the function fails - NULL.
- *
- *      *         Otherwise - a pointer to an array of strings (words).
- *
- *       */
-
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
+ */
 char **strtow(char *str)
+{
+char **strings;
+int index = 0, words, w, letters, l;
+
+if (str == NULL || str[0] == '\0')
+return (NULL);
+
+words = count_words(str);
+if (words == 0)
+return (NULL);
+
+strings = malloc(sizeof(char *) * (words + 1));
+if (strings == NULL)
+return (NULL);
+
+for (w = 0; w < words; w++)
+{
+while (str[index] == ' ')
+index++;
+
+letters = word_len(str + index);
+
+strings[w] = malloc(sizeof(char) * (letters + 1));
+
+if (strings[w] == NULL)
 
 {
+for (; w >= 0; w--)
+free(strings[w]);
 
-		char **strings;
+free(strings);
+return (NULL);
+}
 
-			int index = 0, words, w, letters, l;
+for (l = 0; l < letters; l++)
+strings[w][l] = str[index++];
 
+strings[w][l] = '\0';
+}
+strings[w] = NULL;
 
-
-				if (str == NULL || str[0] == '\0')
-
-							return (NULL);
-
-
-
-					words = count_words(str);
-
-						if (words == 0)
-
-									return (NULL);
-
-
-
-							strings = malloc(sizeof(char *) * (words + 1));
-
-								if (strings == NULL)
-
-											return (NULL);
-
-
-
-									for (w = 0; w < words; w++)
-
-											{
-
-														while (str[index] == ' ')
-
-																		index++;
-
-
-
-																letters = word_len(str + index);
-
-
-
-																		strings[w] = malloc(sizeof(char) * (letters + 1));
-
-
-
-																				if (strings[w] == NULL)
-
-																							{
-
-																											for (; w >= 0; w--)
-
-																																free(strings[w]);
-
-
-
-																														free(strings);
-
-																																	return (NULL);
-
-																																			}
-
-
-
-																						for (l = 0; l < letters; l++)
-
-																										strings[w][l] = str[index++];
-
-
-
-																								strings[w][l] = '\0';
-
-																									}
-
-										strings[w] = NULL;
-
-
-
-											return (strings);
-
+return (strings);
 }
